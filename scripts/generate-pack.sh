60fa670da8c2938f2deb346d11782e87be59b908
melontini <mail@melontini.me>
2025-07-25 12:21:04 +0700

    i don't even remember what was the point of this

--- scripts/generate-pack.sh
+++ scripts/generate-pack.sh
@@ -1,12 +1,9 @@
 mkdir -p output
 
 META_JSON="{\"metaVersion\": 2, \"host\": \"@wetdry.world\", \"exportedAt\": \"$(date -u +"%Y-%m-%dT%H:%M:%SZ")\", \"emojis\": []}"
-PACK_JSON="{\"files\": {}, \"pack\": {}, \"count\": 0}"
 
 EMOJI_ARRAY=$(mktemp)
-FILES_OBJECT=$(mktemp)
 echo "$META_JSON" | jq . > "$EMOJI_ARRAY"
-echo "$PACK_JSON" | jq . > "$FILES_OBJECT"
 
 for FILE in x256/*.png; do
   [ -e "$FILE" ] || continue
@@ -29,11 +26,9 @@ for FILE in x256/*.png; do
     }')
 
   jq --argjson emojiEntry "$EMOJI_ENTRY" '.emojis += [$emojiEntry]' "$EMOJI_ARRAY" > tmp.json && mv tmp.json "$EMOJI_ARRAY"
-  jq --arg key "$FILE_BASE" --arg value "$FILE_NAME" '.files[$key] = $value | .count += 1' "$FILES_OBJECT" > tmp.json && mv tmp.json "$FILES_OBJECT"
 
   cp "$FILE" "output/$FILE_NAME"
 done
 
 mv "$EMOJI_ARRAY" "output/meta.json"
-mv "$FILES_OBJECT" "output/pack.json"
 cp "LICENSE.txt" "output/LICENSE.txt"
